5a9d0c9fef0c988b3759ec9da10005cfdc12aa64
wanderer <mjbecze@gmail.com>
2013-07-19 04:02:47 -0400

    unsquished yt vids

--- challenge/static/challenge/js/claim.js
+++ challenge/static/challenge/js/claim.js
@@ -11,6 +11,8 @@
       yt_id = href.trim().match(/v=(\w+)/)[1];
       iframe = document.createElement('iframe');
       iframe.src = url + yt_id;
+      iframe.height = 236;
+      iframe.width = 420;
       $(link).parent().parent().append(iframe);
     }
     if (QueryString.thanks) {
